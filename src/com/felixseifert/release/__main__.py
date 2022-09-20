c0c4cda3fe8ff8806b151fe1e21f4f4567bdac89
Felix Seifert <mail@felix-seifert.com>
2022-09-20 16:38:46 +0200

    Refactor code to increase readability and testability

--- src/com/felixseifert/release/__main__.py
+++ src/com/felixseifert/release/__main__.py
@@ -1,24 +1,33 @@
-import argparse
+from argparse import Namespace, ArgumentParser
 
 
 def main() -> None:
     args = parse_command_line()
     name = get_name(args)
-    print("Hello, {name}!".format(name=name))
+    print(say_hello(name))
 
 
-def parse_command_line():
-    app_description = 'Say \'Hello\' to a specific person or to the whole world'
-    parser = argparse.ArgumentParser(description=app_description)
-    parser.add_argument('name', type=str, nargs='?',
-                        help='Specify name if you want to greet a specific person')
+def parse_command_line() -> Namespace:
+    parser = create_parser()
     return parser.parse_args()
 
 
-def get_name(args):
+def create_parser() -> ArgumentParser:
+    app_description = 'Say \'Hello\' to a specific person or to the whole world'
+    parser = ArgumentParser(description=app_description)
+    parser.add_argument('name', type=str, nargs='?',
+                        help='Specify name if you want to greet a specific person')
+    return parser
+
+
+def get_name(args: Namespace) -> str:
     if args.name is None:
         return 'World'
     return args.name
+
+
+def say_hello(name: str) -> str:
+    return f"Hello, {name}!"
 
 
 if __name__ == '__main__':
